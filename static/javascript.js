a4447a803bac8238dfbea32ceb6a35b1abfef35a
Laya@Arch <arun.laya@yahoo.com>
2024-04-28 13:43:45 +0530

    check download button

--- static/javascript.js
+++ static/javascript.js
@@ -1,44 +1,44 @@
 console.log("Hello");
 function scrollToSection(sectionId) {
-    const section = document.getElementById(sectionId);
-    if (section) {
-      section.scrollIntoView({ behavior: 'smooth' });
-    }
+  const section = document.getElementById(sectionId);
+  if (section) {
+    section.scrollIntoView({ behavior: "smooth" });
+  }
 }
 document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
-	const dropZoneElement = inputElement.closest(".drop-zone");
+  const dropZoneElement = inputElement.closest(".drop-zone");
 
-	dropZoneElement.addEventListener("click", (e) => {
-		inputElement.click();
-	});
+  dropZoneElement.addEventListener("click", (e) => {
+    inputElement.click();
+  });
 
-	inputElement.addEventListener("change", (e) => {
-		if (inputElement.files.length) {
-			updateThumbnail(dropZoneElement, inputElement.files[0]);
-		}
-	});
+  inputElement.addEventListener("change", (e) => {
+    if (inputElement.files.length) {
+      updateThumbnail(dropZoneElement, inputElement.files[0]);
+    }
+  });
 
-	dropZoneElement.addEventListener("dragover", (e) => {
-		e.preventDefault();
-		dropZoneElement.classList.add("drop-zone--over");
-	});
+  dropZoneElement.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    dropZoneElement.classList.add("drop-zone--over");
+  });
 
-	["dragleave", "dragend"].forEach((type) => {
-		dropZoneElement.addEventListener(type, (e) => {
-			dropZoneElement.classList.remove("drop-zone--over");
-		});
-	});
+  ["dragleave", "dragend"].forEach((type) => {
+    dropZoneElement.addEventListener(type, (e) => {
+      dropZoneElement.classList.remove("drop-zone--over");
+    });
+  });
 
-	dropZoneElement.addEventListener("drop", (e) => {
-		e.preventDefault();
+  dropZoneElement.addEventListener("drop", (e) => {
+    e.preventDefault();
 
-		if (e.dataTransfer.files.length) {
-			inputElement.files = e.dataTransfer.files;
-			updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
-		}
+    if (e.dataTransfer.files.length) {
+      inputElement.files = e.dataTransfer.files;
+      updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
+    }
 
-		dropZoneElement.classList.remove("drop-zone--over");
-	});
+    dropZoneElement.classList.remove("drop-zone--over");
+  });
 });
 
 /**
@@ -48,57 +48,75 @@ document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
  * @param {File} file
  */
 function updateThumbnail(dropZoneElement, file) {
-	let thumbnailElement = dropZoneElement.querySelector(".drop-zone__thumb");
+  let thumbnailElement = dropZoneElement.querySelector(".drop-zone__thumb");
 
-	// First time - remove the prompt
-	if (dropZoneElement.querySelector(".drop-zone__prompt")) {
-		dropZoneElement.querySelector(".drop-zone__prompt").remove();
-	}
+  // First time - remove the prompt
+  if (dropZoneElement.querySelector(".drop-zone__prompt")) {
+    dropZoneElement.querySelector(".drop-zone__prompt").remove();
+  }
 
-	// First time - there is no thumbnail element, so lets create it
-	if (!thumbnailElement) {
-		thumbnailElement = document.createElement("div");
-		thumbnailElement.classList.add("drop-zone__thumb");
-		dropZoneElement.appendChild(thumbnailElement);
-	}
+  // First time - there is no thumbnail element, so lets create it
+  if (!thumbnailElement) {
+    thumbnailElement = document.createElement("div");
+    thumbnailElement.classList.add("drop-zone__thumb");
+    dropZoneElement.appendChild(thumbnailElement);
+  }
 
-	thumbnailElement.dataset.label = file.name;
+  thumbnailElement.dataset.label = file.name;
 
-	// Show thumbnail for image files
-	if (file.type.startsWith("image/")) {
-		const reader = new FileReader();
+  // Show thumbnail for image files
+  if (file.type.startsWith("image/")) {
+    const reader = new FileReader();
 
-		reader.readAsDataURL(file);
-		reader.onload = () => {
-			thumbnailElement.style.backgroundImage = `url('${reader.result}')`;
-		};
-	} else {
-		thumbnailElement.style.backgroundImage = null;
-	}
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      thumbnailElement.style.backgroundImage = `url('${reader.result}')`;
+    };
+  } else {
+    thumbnailElement.style.backgroundImage = null;
+  }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-        document.querySelector('.upl').addEventListener('submit', function(event) {
-            event.preventDefault();
-            var form = event.target;
-            var formData = new FormData(form);
-            fetch(form.action, {
-                method: 'POST',
-                body: formData
-            })
-            .then(response => response.json())
-            .then(data => {
-                var dropdown = document.getElementById('targetColumnDropdown');
-                dropdown.innerHTML = ''; // Clear existing options
-                data.column_names.forEach(function(column) {
-                    var option = document.createElement('option');
-                    option.text = column;
-                    dropdown.add(option);
-                });
-            })
-            .catch(error => console.error('Error:', error));
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".upl").addEventListener("submit", function (event) {
+    event.preventDefault();
+    var form = event.target;
+    var formData = new FormData(form);
+    fetch(form.action, {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        var dropdown = document.getElementById("targetColumnDropdown");
+        dropdown.innerHTML = ""; // Clear existing options
+        data.column_names.forEach(function (column) {
+          var option = document.createElement("option");
+          option.text = column;
+          dropdown.add(option);
         });
+      })
+      .catch((error) => console.error("Error:", error));
+  });
+});
 
-        
-    });
-    
+const dld_btn = document.getElementById("download-btn");
+dld_btn.addEventListener("click", async () => {
+  try {
+    const response = await fetch("/download_code");
+    const blob = await response.blob();
+
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.style.display = "none";
+    a.href = url;
+    a.download = "output.py";
+    document.body.appendChild(a);
+    a.click();
+    window.URL.revokeObjectURL(url);
+
+    console.log("Download Complete");
+  } catch (error) {
+    console.error(`Download Error : $(error)`);
+  }
+});
